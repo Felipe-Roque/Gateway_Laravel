157306c532017bf0de56f9466ad88ddf75acfbf4
Felipe-Roque <felipevieiraroque@hotmail.com>
2017-04-18 17:00:11 -0300

    modelo

---
+++ Meusite/app/CameraPhotos.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Meusite;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CameraPhotos extends Model
+{
+    public $table = 'cameraphotos';
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+    public $fillable = [
+
+    	'name',
+    	'path',
+    	'extension',
+    	'full_file_path',
+    	'captured_at'
+    ];
+}
